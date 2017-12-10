6f910af7dc287cb2c07bde68a033b6d312317851
iryanclarke <clarke_93@hotmail.com>
2017-12-10 17:32:45 -0500

    Working fetching from Marvel API

--- src/index.js
+++ src/index.js
@@ -4,5 +4,6 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App birthday="1993-10-15,1993-10-25"/>, document.getElementById('root'));
+//ReactDOM.render(<App birthday="reactjs"/>, document.getElementById('root'));
 registerServiceWorker();
