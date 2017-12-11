77284d9b3fd476fcc349948a2c7cd6cd4db4e522
iryanclarke <clarke_93@hotmail.com>
2017-12-10 20:39:12 -0500

    Dynamic Hiding/Showing of results, and lots of datepicker updates

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App birthday="1993-10-15,1993-10-25"/>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 //ReactDOM.render(<App birthday="reactjs"/>, document.getElementById('root'));
 registerServiceWorker();
